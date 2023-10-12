eabe53061faf822effe1defd5a7fa5794a41d468
kyeiiih <jaydevsengineer@gmail.com>
2023-10-12 11:05:45 +0300

    Added file upload functionality

--- src/components/CompanyInfo.jsx
+++ src/components/CompanyInfo.jsx
@@ -1,16 +1,72 @@
 import React from "react";
+import "./LoginSignUp.css";
+import "./CompanyInfo.css";
+
+import { useState } from "react";
 
 const CompanyInfo = ({ companyName, logoUrl }) => {
-  const handleSubmitDocument = () => {};
+  const baseUrl = "http://localhost:9000/";
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleSubmitDocument = () => {
+    if (selectedFile) {
+      const formData = new FormData();
+
+      formData.append("file", selectedFile);
+      fetch(baseUrl + "upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("File uploaded successfully");
+          } else {
+            console.error("File upload failed");
+          }
+        })
+        .catch((error) => {
+          console.error("File upload failed:", error);
+        });
+
+      console.log("Selected file:", selectedFile);
+    } else {
+      console.log("No file selected.");
+    }
+  };
 
   const handleViewDocuments = () => {};
 
   return (
     <div className="company-info">
-      <img src={logoUrl} alt="Company Logo" height={200} width={200} />
-      <h1>{companyName}</h1>
-      <button onClick={handleSubmitDocument}>Submit Document</button>
-      <button onClick={handleViewDocuments}>View Submitted Documents</button>
+      <img src={logoUrl} alt="Company Logo" height={20} width={20} />
+      <h3>{companyName}</h3>
+      <div className="file-input">
+        <input
+          type="file"
+          id="document"
+          accept=".pdf, .doc, .docx"
+          onChange={handleFileChange}
+        />
+        {/* <label htmlFor="document">Submit a Document</label> */}
+      </div>
+      <div className="submit-container">
+        <div className="submit" onClick={handleSubmitDocument}>
+          Submit Document
+        </div>
+      </div>
+
+      <div className="submit-container">
+        <div className="submit" onClick={handleViewDocuments}>
+          View Documents
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import CompanyInfo from "./CompanyInfo";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-  const companyName = "Google Inc.";
-  const logoUrl =
-    "https://media.wired.co.uk/photos/606d9a3ba876dd2203a639aa/1:1/w_2000,h_2000,c_limit/wired-uk-google-watching.jpg";
+  const companies = [
+    {
+      name: "URA",
+      logo: "https://play-lh.googleusercontent.com/q8Qr453G_lABl2jVsGpCPxxSWvAukJLorTofg1NqMckEkF61pqmt3877QKMbsr4nJs2S",
+    },
+    {
+      name: "Bank Of Uganda",
+      logo: "https://pbs.twimg.com/profile_images/816542296384696320/A0Q3OTgM_400x400.jpg",
+    },
+    {
+      name: "NSSF",
+      logo: "https://yt3.googleusercontent.com/ytc/APkrFKZESi2jGBKNgZtFBPIgZN3OJLiwpYWszhe1LMtaag=s900-c-k-c0x00ffffff-no-rj",
+    },
+    {
+      name: "POU",
+      logo: "https://pbs.twimg.com/profile_images/1706576411681591296/MT95aRPm_400x400.jpg",
+    },
+  ];
 
   return (
     <div className="dashboard">
-      <CompanyInfo companyName={companyName} logoUrl={logoUrl} />
-      <CompanyInfo companyName={companyName} logoUrl={logoUrl} />
-      <CompanyInfo companyName={companyName} logoUrl={logoUrl} />
-      <CompanyInfo companyName={companyName} logoUrl={logoUrl} />
-      <CompanyInfo companyName={companyName} logoUrl={logoUrl} />
+      {companies.map((company) => (
+        <CompanyInfo
+          key={company.name}
+          companyName={company.name}
+          logoUrl={company.logo}
+        />
+      ))}
     </div>
   );
 };
